9d007b35542dec5c6988e83598d10453657ea4c5
Federico Favaro <fedefava86@gmail.com>
2015-06-06 11:19:42 +0200

    Code refactoring

--- DESERT_Framework/DESERT/mobility/uwrandomlib/uwrandomlib.h
+++ DESERT_Framework/DESERT/mobility/uwrandomlib/uwrandomlib.h
@@ -26,10 +26,18 @@
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Random function library, by GM
-// Last update by MR 25/07/02 -> Pareto distribution
-/*#include<math.h>
-   
+/**
+ * @file   uwrandomlib.h
+ * @version 1.0.0
+ * 
+ * \brief Random function header
+ *
+ * Header of random variable function 
+ */
+
+
+#include <math.h>
+
 #define GENER 3
 
 #define IM1 2147483563L
@@ -50,77 +58,15 @@
 static long int s1[GENER]={1,1,1};
 static long int s2[GENER]={123456789L,123456789L,123456789L};
 static long int iy[GENER]={0,0,0};
-static long int iv[GENER][NTAB];*/
+static long int iv[GENER][NTAB];
 
-#include "randomlib.h"
 
-void SetSeed(long int s,int type){
-  int j;
-  long int k;
 
-  if(s==0)s=1;
-  s1[type]=s;
-  s2[type]=s;
-  for(j=NTAB+7;j>=0;j--){
-    k=s1[type]/IQ1;
-    s1[type]=IA1*(s1[type]-k*IQ1)-k*IR1;
-    if(s1[type]<0)s1[type]+=IM1;
-    if(j<NTAB)iv[type][j]=s1[type];
-  }
-  iy[type]=iv[type][0];
-}
 
-double Rand01(int type){
-  int j;
-  long int k;
-  double t;
+void SetSeed(long int s,int type);
 
-  k=s1[type]/IQ1;
-  s1[type]=IA1*(s1[type]-k*IQ1)-k*IR1;
-  if(s1[type]<0)s1[type]+=IM1;
+double Rand01(int type);
 
-  k=s2[type]/IQ2;
-  s2[type]=IA2*(s2[type]-k*IQ2)-k*IR2;
-  if(s2[type]<0)s2[type]+=IM2;
+double Gauss(double m,double sigma,int type);
 
-  j=iy[type]/NDIV;
-  iy[type]=iv[type][j]-s2[type];
-  iv[type][j]=s1[type];
-  if(iy[type]<1)iy[type]+=IMM1;
-  if((t=AM*iy[type])>RNMX)
-    return RNMX;
-  else
-    return t;
-}
-
-// Generate gaussian distributed numbers with mean m and stdev sigma
-double Gauss(double m,double sigma,int type){
-  static int cache=0;
-  static float v1;
-  float v2,w,y;
-
-  if(cache){
-    cache=0;
-    return v1*sigma+m;
-  }
-  else{
-    do{
-      v1=2.0*Rand01(type)-1.0;
-      v2=2.0*Rand01(type)-1.0;
-      w=(v1*v1)+(v2*v2);
-    }while(w>1.0);
-    y=sqrt((-2.0*log(w))/w);
-    v1=v1*y;
-    cache=1;
-    return v2*y*sigma+m;
-  }
-}
-
-// Generate Pareto distributed numbers
-// FX(x)=Prob(X<=x)
-// FX(x)=1-((beta+x)/beta)^-alpha	x>=0
-// FX(x)=0				otherwise
-double Pareto(double alpha, double beta, int type)
-{	
-	return( beta*(pow( (1.-Rand01(type)),alpha)-1.) );
-}
+double Pareto(double alpha, double beta, int type);
