3a780a6967bdb151c9b3a55cc2940de3d3c5a21f
Federico Favaro <fedefava86@gmail.com>
2015-06-28 15:31:11 +0200

    Fix headers of files in application layers and channel layers
    Copyright date, authors, etc

--- DESERT_Framework/DESERT/application/uwApplication/uwApplication_UDP_socket.cc
+++ DESERT_Framework/DESERT/application/uwApplication/uwApplication_UDP_socket.cc
@@ -1,4 +1,19 @@
 //
+// Copyright (c) 2015 Regents of the SIGNET lab, University of Padova.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the University of Padova (SIGNET lab) nor the 
+//    names of its contributors may be used to endorse or promote products 
+//    derived from this software without specific prior written permission.
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
 // TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
@@ -12,9 +27,12 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-/* 
- * File:   uwApplication_module.cc
- * Author: Federico Favaro
+/**
+ * @file   uwApplication_UDP_socket.cc
+ * @author Federico Favaro
+ * @version 1.1.0
+ *
+ * \brief Provides the implementation of the UDP socket between the protocol and any other application media.
  *
  */
 
