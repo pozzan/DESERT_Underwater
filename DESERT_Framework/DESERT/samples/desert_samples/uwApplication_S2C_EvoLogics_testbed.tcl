ee31c8773f810b2acb03ed6ed2c63124c8cd8a59
Federico Favaro <fedefava86@gmail.com>
2016-02-14 13:58:09 +0100

    Update tcl samples with new variables

--- DESERT_Framework/DESERT/samples/desert_samples/uwApplication_S2C_EvoLogics_testbed.tcl
+++ DESERT_Framework/DESERT/samples/desert_samples/uwApplication_S2C_EvoLogics_testbed.tcl
@@ -236,13 +236,14 @@ Module/UW/APPLICATION set EXP_ID_ $opt(exp_ID)
 
 # variables for the S2C modem's interface
 #####
-Module/UW/MPhy_modem/S2C set period_ 			        1
-Module/UW/MPhy_modem/S2C set debug_ 			        1
-Module/UW/MPhy_modem/S2C set log_                       0
-Module/UW/MPhy_modem/S2C set SetModemID_	 	        0
-Module/UW/MPhy_modem/S2C set UseKeepOnline_             1
-Module/UW/MPhy_modem/S2C set ResetQueue_ 1
+Module/UW/MPhy_modem/S2C set period_ 		1
+Module/UW/MPhy_modem/S2C set debug_ 		1
+Module/UW/MPhy_modem/S2C set log_               0
+Module/UW/MPhy_modem/S2C set SetModemID_	0
+Module/UW/MPhy_modem/S2C set UseKeepOnline_	1
+Module/UW/MPhy_modem/S2C set DeafTime_ 		2
 #######
+
 
 ################################
 # Procedure(s) to create nodes #
