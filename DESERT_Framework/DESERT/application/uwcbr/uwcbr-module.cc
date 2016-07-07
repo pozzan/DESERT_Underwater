7e01ea4362caf238f092eac93f9665770855da80
Riccardo Zanol <ricc@zanol.eu>
2016-07-07 21:56:04 +0200

    Remove two inherited functions that are not changed

--- DESERT_Framework/DESERT/application/uwcbr/uwcbr-module.cc
+++ DESERT_Framework/DESERT/application/uwcbr/uwcbr-module.cc
@@ -194,13 +194,6 @@ int UwCbrModule::command(int argc, const char*const* argv) {
     return Module::command(argc, argv);
 }
 
-int UwCbrModule::crLayCommand(ClMessage* m) {
-    switch (m->type()) {
-        default:
-            return Module::crLayCommand(m);
-    }
-}
-
 void UwCbrModule::initPkt(Packet* p) {
     hdr_cmn* ch = hdr_cmn::access(p);
     ch->uid()   = uidcnt_++;
@@ -274,12 +267,6 @@ void UwCbrModule::transmit() {
 void UwCbrModule::stop() {
     sendTmr_.force_cancel();
 }
-
-void UwCbrModule::recv(Packet* p, Handler* h) {
-//    hdr_cmn* ch = hdr_cmn::access(p);
-    recv(p);
-}
-
 
 void UwCbrModule::recv(Packet* p) {
     hdr_cmn* ch = hdr_cmn::access(p);
