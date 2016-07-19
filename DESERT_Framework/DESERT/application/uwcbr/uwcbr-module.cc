59f3b7558e1b7d90a314a68f2ff7652696d73fb7
Riccardo Zanol <ricc@zanol.eu>
2016-07-19 16:15:41 +0200

    Move the SN check in the initPkt function

--- DESERT_Framework/DESERT/application/uwcbr/uwcbr-module.cc
+++ DESERT_Framework/DESERT/application/uwcbr/uwcbr-module.cc
@@ -42,6 +42,7 @@
 #include <iostream>
 #include <rng.h>
 #include <sstream>
+#include <stdexcept>
 #include <stdint.h>
 
 std::string logprefix(const std::string &func) {
@@ -288,6 +289,9 @@ int UwCbrModule::command(int argc, const char*const* argv) {
 }
 
 void UwCbrModule::initPkt(Packet* p) {
+    if (txsn >= numeric_limits<sn_t>::max())
+	throw overflow_error("Reached the max value for the SN");
+
     hdr_cmn* ch = hdr_cmn::access(p);
     ch->ptype() = PT_UWCBR;
     ch->size()  = pktSize_;
@@ -379,10 +383,6 @@ void UwCbrModule::sendPkt(Packet *p, double delay) {
 }
 
 void UwCbrModule::sendPkt() {
-    if (txsn >= numeric_limits<sn_t>::max()) {
-	cerr << "Reached the max value for the SN, " << numeric_limits<sn_t>::max()-1 << endl;
-	return;
-    }
     double delay      = 0;
     Packet* p         = Packet::alloc();
     initPkt(p);
